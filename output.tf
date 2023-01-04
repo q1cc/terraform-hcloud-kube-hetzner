5f8d95e6a38b3b95e138c91e5777c61f120b654a
Markus Dangl <663399+xicesky@users.noreply.github.com>
2023-01-05 00:33:11 +0100

    Disable most stuff for debugging

--- output.tf
+++ output.tf
@@ -17,16 +17,6 @@ output "agents_public_ipv4" {
   description = "The public IPv4 addresses of the agent servers."
 }
 
-output "ingress_public_ipv4" {
-  description = "The public IPv4 address of the Hetzner load balancer"
-  value       = local.has_external_load_balancer ? module.control_planes[keys(module.control_planes)[0]].ipv4_address : data.hcloud_load_balancer.cluster[0].ipv4
-}
-
-output "ingress_public_ipv6" {
-  description = "The public IPv6 address of the Hetzner load balancer"
-  value       = (local.has_external_load_balancer || var.load_balancer_disable_ipv6) ? null : data.hcloud_load_balancer.cluster[0].ipv6
-}
-
 # Keeping for backward compatibility
 output "kubeconfig_file" {
   value       = local.kubeconfig_external
