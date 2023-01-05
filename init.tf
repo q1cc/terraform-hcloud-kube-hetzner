6ab0878db3f91947a48c9f17dd3a2f6187e77395
Markus Dangl <663399+xicesky@users.noreply.github.com>
2023-01-05 12:32:01 +0100

    Re-enable remote_exec provisioners.

--- init.tf
+++ init.tf
@@ -221,63 +221,69 @@ resource "null_resource" "kustomization" {
     destination = "/var/post_install/rancher.yaml"
   }
 
-  # # Deploy secrets, logging is automatically disabled due to sensitive variables
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "set -ex",
-  #     "sleep 30",
-  #     "kubectl -n kube-system create secret generic hcloud --from-literal=token=${var.hcloud_token} --from-literal=network=${hcloud_network.k3s.name} --dry-run=client -o yaml | kubectl apply -f -",
-  #     "kubectl -n kube-system create secret generic hcloud-csi --from-literal=token=${var.hcloud_token} --dry-run=client -o yaml | kubectl apply -f -",
-  #   ]
-  # }
+  # Deploy secrets, logging is automatically disabled due to sensitive variables
+  provisioner "remote-exec" {
+    inline = [
+      "set -ex",
+      "sleep 30",
+      "kubectl -n kube-system create secret generic hcloud --from-literal=token=${var.hcloud_token} --from-literal=network=${hcloud_network.k3s.name} --dry-run=client -o yaml | kubectl apply -f -",
+      "kubectl -n kube-system create secret generic hcloud-csi --from-literal=token=${var.hcloud_token} --dry-run=client -o yaml | kubectl apply -f -",
+    ]
+  }
 
-  # # Deploy our post-installation kustomization
-  # provisioner "remote-exec" {
-  #   inline = concat([
-  #     "set -ex",
+  # Deploy our post-installation kustomization
+  provisioner "remote-exec" {
+    inline = concat([
+      "set -ex",
 
-  #     # This ugly hack is here, because terraform serializes the
-  #     # embedded yaml files with "- |2", when there is more than
-  #     # one yamldocument in the embedded file. Kustomize does not understand
-  #     # that syntax and tries to parse the blocks content as a file, resulting
-  #     # in weird errors. so gnu sed with funny escaping is used to
-  #     # replace lines like "- |3" by "- |" (yaml block syntax).
-  #     # due to indendation this should not changes the embedded
-  #     # manifests themselves
-  #     "sed -i 's/^- |[0-9]\\+$/- |/g' /var/post_install/kustomization.yaml",
+      # This ugly hack is here, because terraform serializes the
+      # embedded yaml files with "- |2", when there is more than
+      # one yamldocument in the embedded file. Kustomize does not understand
+      # that syntax and tries to parse the blocks content as a file, resulting
+      # in weird errors. so gnu sed with funny escaping is used to
+      # replace lines like "- |3" by "- |" (yaml block syntax).
+      # due to indendation this should not changes the embedded
+      # manifests themselves
+      "sed -i 's/^- |[0-9]\\+$/- |/g' /var/post_install/kustomization.yaml",
 
-  #     # Wait for k3s to become ready (we check one more time) because in some edge cases,
-  #     # the cluster had become unvailable for a few seconds, at this very instant.
-  #     <<-EOT
-  #     timeout 180 bash <<EOF
-  #       until [[ "\$(kubectl get --raw='/readyz' 2> /dev/null)" == "ok" ]]; do
-  #         echo "Waiting for the cluster to become ready..."
-  #         sleep 2
-  #       done
-  #     EOF
-  #     EOT
-  #     ]
-  #     ,
+      # Wait for k3s to become ready (we check one more time) because in some edge cases,
+      # the cluster had become unvailable for a few seconds, at this very instant.
+      <<-EOT
+      timeout 180 bash <<EOF
+        until [[ "\$(kubectl get --raw='/readyz' 2> /dev/null)" == "ok" ]]; do
+          echo "Waiting for the cluster to become ready..."
+          sleep 2
+        done
+      EOF
+      EOT
+      ,
 
-  #     [
-  #       # Ready, set, go for the kustomization
-  #       "kubectl apply -k /var/post_install",
-  #       "echo 'Waiting for the system-upgrade-controller deployment to become available...'",
-  #       "kubectl -n system-upgrade wait --for=condition=available --timeout=180s deployment/system-upgrade-controller",
-  #       "sleep 5", # important as the system upgrade controller CRDs sometimes don't get ready right away, especially with Cilium.
-  #       "kubectl -n system-upgrade apply -f /var/post_install/plans.yaml"
-  #     ],
-  #     local.has_external_load_balancer ? [] : [
-  #       <<-EOT
-  #     timeout 180 bash <<EOF
-  #     until [ -n "\$(kubectl get -n kube-system service/${lookup(local.ingress_controller_service_names, local.ingress_controller)} --output=jsonpath='{.status.loadBalancer.ingress[0].ip}' 2> /dev/null)" ]; do
-  #         echo "Waiting for load-balancer to get an IP..."
-  #         sleep 2
-  #     done
-  #     EOF
-  #     EOT
-  #   ])
-  # }
+      # Check that /var/post_install exists and is a directory containing kustomization files
+      <<-EOT
+      ls -lad /var/post_install /var/post_install/rancher.yaml /var/post_install/kustomization.yaml
+      EOT
+      ]
+      ,
+
+      [
+        # Ready, set, go for the kustomization
+        "kubectl apply -k /var/post_install",
+        "echo 'Waiting for the system-upgrade-controller deployment to become available...'",
+        "kubectl -n system-upgrade wait --for=condition=available --timeout=180s deployment/system-upgrade-controller",
+        "sleep 5", # important as the system upgrade controller CRDs sometimes don't get ready right away, especially with Cilium.
+        "kubectl -n system-upgrade apply -f /var/post_install/plans.yaml"
+      ],
+      local.has_external_load_balancer ? [] : [
+        <<-EOT
+      timeout 180 bash <<EOF
+      until [ -n "\$(kubectl get -n kube-system service/${lookup(local.ingress_controller_service_names, local.ingress_controller)} --output=jsonpath='{.status.loadBalancer.ingress[0].ip}' 2> /dev/null)" ]; do
+          echo "Waiting for load-balancer to get an IP..."
+          sleep 2
+      done
+      EOF
+      EOT
+    ])
+  }
 
   depends_on = [
     null_resource.first_control_plane,
